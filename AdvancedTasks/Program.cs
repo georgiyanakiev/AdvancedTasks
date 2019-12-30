0477265ad1c850db4193dd6d6704653e3281df88
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-12-30 15:24:06 +0200

    Add project files.

---
+++ AdvancedTasks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AdvancedTasks
+{
+   public static class Program
+    {
+       public static void ThreadMethod()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Thread process: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+
+
+       public static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i<10; i++)
+            {
+                Console.WriteLine("This is main Thread!");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.ReadLine();
+        }
+    }
+}
